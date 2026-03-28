b8fd677b630d78169c32d5509e2d4c46a293f21e
Alberto <alberrto.ramos@gmail.com>
2026-03-28 17:40:33 -0600

    Ajuste general de vistas

---
+++ app/src/main/java/com/example/dsm_desafio02/dialog_add_task.kt
@@ -0,0 +1,20 @@
+package com.example.dsm_desafio02
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class dialog_add_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dialog_add_task)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
